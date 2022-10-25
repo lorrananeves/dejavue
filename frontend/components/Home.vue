7f4bd8681e0c853a6264ab6dc8db537e984a3a87
Lorrana <lorrana.neves@buser.com.br>
2022-10-25 10:51:36 -0300

    feito v-list simplão 2

--- frontend/components/Home.vue
+++ frontend/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <timeline></timeline>
+  <timeline :tweets="tweets"></timeline>
 </template>
 
 <script>
@@ -7,6 +7,7 @@
 import timeline from '~/components/Timeline.vue'
 
 export default {
+    props: ['tweets'],
     components: {
         timeline
     },
